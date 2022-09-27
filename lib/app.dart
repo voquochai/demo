5700f24df0ff748f2c256e7a543621f6d8a219d2
vmcuongit <89331418+vmcuongit@users.noreply.github.com>
2022-09-27 13:48:53 +0700

    start project

---
+++ lib/app.dart
@@ -0,0 +1,64 @@
+import 'screens/cakery_page.dart';
+import 'widgets/bottom_navigation_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color(0xFF545D68),
+          ),
+          onPressed: () {},
+        ),
+        title: Text(
+          'Bahri Cakery',
+          style: TextStyle(
+            fontSize: 24,
+            color: const Color(0xFF545D68),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.notifications_none,
+              color: Color(0xFF545D68),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        children: [
+          SizedBox(height: 8),
+          Text('Menu',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: MediaQuery.of(context).size.height - 160,
+            width: double.infinity,
+            child: CakeryPage(),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFFF17532),
+        child: Icon(
+          Icons.shopping_bag_sharp,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBarWidget(),
+    );
+  }
+}
